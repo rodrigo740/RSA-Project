6f3b04156ff2865d2c27703d9d18b69e02ada9b4
rodrigo740 <rodrigomartins@ua.pt>
2022-05-14 23:43:20 +0100

    more

--- simulator.py
+++ simulator.py
@@ -1,13 +1,10 @@
-import requests
-import csv
 import paho.mqtt.client as mqtt
 import signal
 import gpxpy
 import gpxpy.gpx
-import logging
-import threading
 import time
 import multiprocessing
+import json
 
 client1 = mqtt.Client()
 client2 = mqtt.Client()
@@ -37,7 +34,8 @@ def on_connect1(client, userdata, flags, rc):
 # The callback for when a PUBLISH message is received from the server.
 def on_message1(client, userdata, msg):
     print("RSU got: ")
-    print(msg.topic+" "+str(msg.payload))
+    payload = json.loads(msg.payload)
+    print(payload)
 
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -53,7 +51,8 @@ def on_connect2(client, userdata, flags, rc):
 
 def on_message2(client, userdata, msg):
     print("OBU 1: ")
-    print(msg.topic+" "+str(msg.payload))
+    payload = json.loads(msg.payload)
+    print(payload)
 
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -72,7 +71,8 @@ def on_connect3(client, userdata, flags, rc):
 # The callback for when a PUBLISH message is received from the server.
 def on_message3(client, userdata, msg):
     print("OBU 2: ")
-    print(msg.topic+" "+str(msg.payload))
+    payload = json.loads(msg.payload)
+    print(payload)
 
 
 def setupMqtt():
@@ -81,21 +81,21 @@ def setupMqtt():
     client1.on_message = on_message1
 
     client1.connect("192.168.98.10")
-    # client1.loop_start()
+    client1.loop_start()
 
     # OBU1 client
     client2.on_connect = on_connect2
     client2.on_message = on_message2
 
     client2.connect("192.168.98.20")
-    # client2.loop_start()
+    client2.loop_start()
 
     # OBU2 client
     client3.on_connect = on_connect3
     client3.on_message = on_message3
 
     client3.connect("192.168.98.30")
-    # client3.loop_start()
+    client3.loop_start()
 
 
 def parseGPX(gpxFile):
@@ -117,31 +117,68 @@ def parseGPX(gpxFile):
     return coordsList
 
 
-def thread_function(name):
-    logging.info("Thread %s: starting", name)
-    time.sleep(2)
-    logging.info("Thread %s: finishing", name)
-
-
 def startSimul(currClient, coordsList):
     print("Client: " + str(currClient) + " has started the simulation")
 
+    if currClient == 0:
+        # RSU client
+        client1.on_connect = on_connect1
+        client1.on_message = on_message1
+
+        client1.connect("192.168.98.10")
+        client1.loop_start()
+    elif currClient == 1:
+
+        # OBU1 client
+        client2.on_connect = on_connect2
+        client2.on_message = on_message2
+
+        client2.connect("192.168.98.20")
+        client2.loop_start()
+    elif currClient == 2:
+
+        # OBU2 client
+        client3.on_connect = on_connect3
+        client3.on_message = on_message3
+
+        client3.connect("192.168.98.30")
+        client3.loop_start()
+
     # Start mqtt loop
-    clientsList[currClient].loop_start
+    # clientsList[currClient].loop_start
     if currClient > 1:
         time.sleep(5)
 
-    myID = int(currClient) + 1
+    if currClient != 0:
+        myID = int(currClient) + 1
 
-    msg_payload = "{\"accEngaged\":true,\"acceleration\":0,\"altitude\":800001,\"altitudeConf\":15,\"brakePedal\":true,\"collisionWarning\":true,\"cruiseControl\":true,\"curvature\":1023,\"driveDirection\":\"FORWARD\",\"emergencyBrake\":true,\"gasPedal\":false,\"heading\":3601,\"headingConf\":127,\"latitude\":400000000,\"length\":100,\"longitude\":-80000000,\"semiMajorConf\":4095,\"semiMajorOrient\":3601,\"semiMinorConf\":4095,\"specialVehicle\":{\"publicTransportContainer\":{\"embarkationStatus\":false}},\"speed\":16383,\"speedConf\":127,\"speedLimiter\":true,\"stationID\":" + \
-        str(myID) + ",\"stationType\":15,\"width\":30,\"yawRate\":0}"
+        for coords in coordsList:
+            lat = coords[0]*pow(10, 7)
+            lng = coords[1]*pow(10, 7)
+            msg_payload = "{\"accEngaged\":true,\"acceleration\":0,\"altitude\":800001,\"altitudeConf\":15,\"brakePedal\":true,\"collisionWarning\":true,\"cruiseControl\":true,\"curvature\":1023,\"driveDirection\":\"FORWARD\",\"emergencyBrake\":true,\"gasPedal\":false,\"heading\":3601,\"headingConf\":127,\"latitude\":" + \
+                str(lat) + ",\"length\":100,\"longitude\":" + \
+                str(lng) + ",\"semiMajorConf\":4095,\"semiMajorOrient\":3601,\"semiMinorConf\":4095,\"specialVehicle\":{\"publicTransportContainer\":{\"embarkationStatus\":false}},\"speed\":16383,\"speedConf\":127,\"speedLimiter\":true,\"stationID\":" + \
+                str(myID) + ",\"stationType\":15,\"width\":30,\"yawRate\":0}"
 
-    clientsList[currClient].publish(
-        topic="vanetza/in/cam", payload=msg_payload)
+            clientsList[currClient].publish(
+                topic="vanetza/in/cam", payload=msg_payload)
+            time.sleep(0.1)
+
+    if currClient == 0:
+        # RSU client
+        client1.loop_forever()
+    elif currClient == 1:
+        # OBU1 client
+        client2.loop()
+    elif currClient == 2:
+        # OBU2 client
+        client1.disconnect()
+        client2.disconnect()
+        client3.disconnect()
 
 
 def main():
-    setupMqtt()
+    # setupMqtt()
     gpxFile = "route-1.gpx"
     coordsList = parseGPX(gpxFile)
 
@@ -165,8 +202,9 @@ def main():
     # both processes finished
     print("Done!")
 
-    while(True):
-        print("done")
+    client1.disconnect
+    client2.disconnect
+    client3.disconnect
 
 
 if __name__ == "__main__":
